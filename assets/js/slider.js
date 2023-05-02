b7a3ad2e8b0a7d6ea7f8340922f74c62761bce22
Hussnain Ahmad <hussnainahmad1606@gmail.com>
2023-05-03 01:18:15 +0500

    Add testimonials page

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,7 +1,32 @@
 console.log("Slider Running");
+
+// Slider at header
 $('.slide').slick({
     infinite: true,
   slidesToShow: 1,
   autoplay: true,
   autoplaySpeed: 1000,
+});
+
+
+
+
+
+// Testimonials section
+$('.testimonials').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: 1000,
+  dots: true,
+  useCSS: true,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+  ]
 });
